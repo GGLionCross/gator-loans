0ea8e20dd90c9973a1a0e0c90c71941daff372fe
Lionel Quiambao <lionelquiambao@gmail.com>
2022-06-11 22:18:32 -0700

    Created Quasar project with Vite and adding Firebase

---
+++ src/boot/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjcsCn5B8HWE8Bn0xAHqV6mI8KeQG6ff4",
+  authDomain: "gator-loans.firebaseapp.com",
+  databaseURL: "https://gator-loans-default-rtdb.firebaseio.com",
+  projectId: "gator-loans",
+  storageBucket: "gator-loans.appspot.com",
+  messagingSenderId: "1087255148409",
+  appId: "1:1087255148409:web:b77da603e5fd2468e561a8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
